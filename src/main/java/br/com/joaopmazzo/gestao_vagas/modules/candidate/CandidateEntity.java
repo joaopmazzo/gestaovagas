05edec2b95dda570ab35582f48215b7aed7127cc
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-29 17:06:59 -0300

    Aula 'Inserindo validação'

--- src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,9 @@ package br.com.joaopmazzo.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +18,14 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
     private String username;
+    
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
+
+    @Size(min = 6, max = 20)
     private String password;
     private String description;
     private String curriculum;
