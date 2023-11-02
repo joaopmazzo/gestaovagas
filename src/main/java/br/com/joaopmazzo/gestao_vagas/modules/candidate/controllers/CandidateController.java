d24d45d94c4a9e841edb89a03b9b7f2fbc3026e4
João Paulo Mazzo <mazzojp@gmail.com>
2023-11-02 10:23:09 -0300

    Aula 'Validando usuário já existente'

--- src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,15 +1,14 @@
 package br.com.joaopmazzo.gestao_vagas.modules.candidate.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.joaopmazzo.gestao_vagas.exceptions.UserFoundException;
 import br.com.joaopmazzo.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.joaopmazzo.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,6 +20,11 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+            .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+            .ifPresent((user) -> {
+                throw new UserFoundException();
+            });
         return this.candidateRepository.save(candidateEntity);
     }
     
