1047500ec4d1eccbc61677a59edd05e7a626392c
João Paulo Mazzo <=>
2023-12-25 13:13:31 -0300

    Aula: Refatorando JobController

--- src/main/java/br/com/joaopmazzo/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/joaopmazzo/gestao_vagas/security/SecurityFilter.java
@@ -1,22 +1,51 @@
 package br.com.joaopmazzo.gestao_vagas.security;
 
+import br.com.joaopmazzo.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
+    private final JWTProvider jwtProvider;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest,
-                                    HttpServletResponse httpServletResponse,
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String header = httpServletRequest.getHeader("Authorization");
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        SecurityContextHolder.getContext().setAuthentication(null); // serve para limpar sujeiras
+        String header = request.getHeader("Authorization");
+
+        if (Objects.nonNull(header)) {
+            String subjectToken = jwtProvider.validateToken(header);
+
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("company_id", subjectToken);
+
+            // Serve para o spring sempre validar se o usuario está autenticado e se possui as roles
+            // necessárias
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    subjectToken, null, Collections.emptyList()
+            );
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
