d1ad66c3b50f83365e1f6cb99a6da20e1eef30b1
João Paulo Mazzo <=>
2024-02-19 19:01:53 -0300

    Aula: Criando SwaggerConfig

---
+++ src/main/java/br/com/joaopmazzo/gestao_vagas/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.joaopmazzo.gestao_vagas.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        var INFO = new Info()
+                .title("Gestão de Vagas")
+                .description("API responsável pela gestão de vagas")
+                .version("1");
+
+        return new OpenAPI()
+                .info(INFO)
+                .schemaRequirement("jwt_auth", createSecurityScheme());
+
+//        para atribuir em todas as rotas
+        
+//        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+//                .components(new Components().addSecuritySchemes("Bearer Authentication", createSecurityScheme()))
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .name("jwt_auth")
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER);
+    }
+
+}
