14930e4543547179cf177ffb9ef38bba52cb669b
João Paulo Mazzo <=>
2024-02-20 11:21:33 -0300

    Aula: Criando entidade de Apply Job

--- src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/usecases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/joaopmazzo/gestao_vagas/modules/candidate/usecases/ApplyJobCandidateUseCase.java
@@ -2,6 +2,7 @@ package br.com.joaopmazzo.gestao_vagas.modules.candidate.usecases;
 
 import br.com.joaopmazzo.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.joaopmazzo.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.joaopmazzo.gestao_vagas.modules.candidate.repositories.ApplyJobRepository;
 import br.com.joaopmazzo.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import br.com.joaopmazzo.gestao_vagas.modules.company.repositories.JobRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class ApplyJobCandidateUseCase {
 
     private final CandidateRepository candidateRepository;
     private final JobRepository jobRepository;
+    private final ApplyJobRepository applyJobRepository;
 
     public void execute(UUID idCandidate, UUID idJob) {
         candidateRepository.findById(idCandidate).orElseThrow(UserNotFoundException::new);
